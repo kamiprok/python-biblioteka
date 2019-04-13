68c9b9884f5092f8e8aa239238be1155ef9a4256
Sylwester Kwiatkowski <s.kwiatkowski1992@outlook.com>
2019-04-13 18:34:22 +0200

    Dodaj pliki projektów.

---
+++ BibliotekaSQL/DBConnect.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+
+namespace BibliotekaSQL
+{
+    class DBConnect
+    {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string user;
+        private string password;
+        private string port;
+        private string sslM;
+        private string connectionString;
+
+        public DBConnect()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            server = "localhost";
+            database = "biblioteka";
+            user = "root";
+            password = "admin123";
+            port = "3306";
+            sslM = "none";
+
+            connectionString = String.Format("Server={0};UserId={1};Database={2}", server, user, database);
+            //connectionString = String.Format("server={0};port={1};uid={2}; pwd={3}; database={4}; SslMode={5}", server, port, user, password, database, sslM);
+
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Połączenie nawiązane");
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                switch (ex.Number)
+                {
+                    case 0:
+                        Console.WriteLine("Nie można połączyć się z serwerem.");
+                        break;
+                    case 1045:
+                        Console.WriteLine("Niewłaściwa nazwa użytkownika/hasło, spróbuj ponownie");
+                        break;
+                }
+                return false;
+            }
+        }
+        private bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch(MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+        public void conexion()
+        {
+            try
+            {
+                connection.Open();
+
+                Console.WriteLine("successful connection");
+
+                connection.Close();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message + connectionString);
+            }
+        }
+        public void Insert(string tablename, List<String> tables, List<String> values)
+        {
+            string table = "";
+            string value = "";
+            foreach (string x in tables) {
+                table = table + x + ",";
+            }
+            table = table.Remove(table.Length - 1, 1);
+            foreach (string x in values)
+            {
+                value = value + "'" + x + "'" + ",";
+            }
+            value = value.Remove(value.Length - 1, 1);
+
+            string query = "INSERT INTO " + tablename + "(" + table + ") VALUES(" + value + ");";
+            Console.WriteLine(query);
+            if(this.OpenConnection() == true)
+            {
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                this.CloseConnection();
+            }
+            else
+            {
+                Console.WriteLine("Brak połączenia");
+            }
+        }
+        public void Update() { }
+        //public void Update() { }
+        public void Delete() { }
+        //public List<string> [] Select() { }
+        //public int Count() { }
+        public void Backup() { }
+        public void Restore() { }
+
+    }
+}
---
+++ BibliotekaSQL/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace BibliotekaSQL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1. Dodaj rekord");
+            Console.WriteLine("2. Usuń rekord");
+
+            DBConnect conn = new DBConnect();
+
+            List<String> tabele = new List<string>();
+            List<String> wartosci = new List<string>();
+
+            tabele.Add("id_autora");
+            tabele.Add("imie");
+            tabele.Add("nazwisko");
+            tabele.Add("narodowosc");
+            tabele.Add("liczba_dziel");
+            tabele.Add("zyciorys");
+
+            wartosci.Add("10000");
+            wartosci.Add("Henryk");
+            wartosci.Add("Sienkiewicz");
+            wartosci.Add("Polska");
+            wartosci.Add("21");
+            wartosci.Add("Krótki opis jego życia");
+
+            conn.Insert("autorzy", tabele, wartosci);
+
+            Console.WriteLine("Dopisane");
+
+            Console.ReadLine();
+        }
+    }
+}
