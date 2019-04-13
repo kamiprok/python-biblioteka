fb42799f1920f39c2fa1822d0909454b41251712
Sylwester Kwiatkowski <s.kwiatkowski1992@gmail.com>
2019-04-13 19:24:36 +0200

    test

---
+++ BibliotekaSQL/Models.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BibliotekaSQL
+{
+    public class Models
+    {
+        string[] autorzy = { "imie", "nazwisko", "narodowosc", "liczba_dziel", "zyciorys" };
+        string[] ksiazki = { "id_autora", "wydawnictwo", "gatunek", "tytul" };
+        string[] wypozyczenia = { "id_czytelnika", "id_ksiazki", "data_zamowienia", "data_wypozyczenia", "data_zwrotu", "status" };
+        string[] wydawnictwa = { "nazwa", "kraj", "miasto" };
+        string[] gatunki = { "nazwa" };
+    }
+}
