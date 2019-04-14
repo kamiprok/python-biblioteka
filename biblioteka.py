2003ea788fa012d1b101a43638fa28684eed9641
kamson <kamiprok@gmail.com>
2019-04-14 17:44:30 +0200

    to fix error on exit, still 5% complete

--- biblioteka.py
+++ biblioteka.py
@@ -6,6 +6,35 @@ print(mydb)
 
 mycursor = mydb.cursor()
 
+query = 'SHOW TABLES'
+mycursor.execute(query)
+lista_tabele = []
+for i in mycursor:
+    lista_tabele.append(i[0])
+
+print(lista_tabele)
+if 'autorzy' in lista_tabele:
+    print(f'Znaleziono "autorzy" na pozycji {lista_tabele.index("autorzy")}')
+
+
+query = 'SELECT * from autorzy'
+list_autor = [mycursor.execute(query)]
+list_autor_2 = []
+print(list_autor)
+for x in mycursor:
+    list_autor_2.append(x)
+print(list_autor_2[1][2])
+
+query = 'Show columns from autorzy'
+cos = mycursor.execute(query)
+lista_autorzy = []
+for i in mycursor:
+    lista_autorzy.append(i[0])
+print(lista_autorzy)
+if 'id_autora' in lista_autorzy:
+    print(f'Znaleziono "id_autora" na pozycji {lista_autorzy.index("id_autora")}')
+input('')
+
 mycursor.execute('SHOW DATABASES')
 
 for x in mycursor:
--- bibliotekarz.py
+++ bibliotekarz.py
@@ -7,11 +7,18 @@ mycursor = mydb.cursor()
 dash = '=' * 40
 
 
+query = 'Show columns from autorzy'
+cos = mycursor.execute(query)
+lista_autorzy = []
+for i in mycursor:
+    lista_autorzy.append(i[0])
+
+
 def wyszukaj():
     os.system('cls')
     print('BIBLIOTEKARZ (Beta)')
-    print('\nCzytelnik - Wyszukaj\n')
-    print('Wybierz czego szukasz:')
+    print('\nMenu - Czytelnik - Wyszukaj\n')
+    print('Wybierz czego szukasz:\n')
     print('1. Autora')
     print('2. Książki')
     choice = input('\nOption: ')
@@ -31,18 +38,17 @@ def wyszukaj_autora():
     try:
         os.system('cls')
         print('BIBLIOTEKARZ (Beta)')
-        print('\nCzytelnik - Wyszukaj - Autora\n')
-        print('Wybierz kryterium szukania:')
+        print('\nMenu - Czytelnik - Wyszukaj - Autora\n')
+        print('Wybierz kryterium szukania:\n')
         print('1. ID autora')
         print('2. Imie')
         print('3. Nazwisko')
-        # print('Opis')
         print('\nE. Exit')
         choice = input('\nOption: ')
         if choice == '1':
             os.system('cls')
             id_autora = input('Wpisz ID autora:')
-            query = (f'SELECT id_autora, imie, nazwisko FROM autorzy WHERE id_autora={id_autora}')
+            query = f'SELECT {lista_autorzy[0]}, imie, nazwisko FROM autorzy WHERE {lista_autorzy[0]}={id_autora}'
             mycursor.execute(query)
             print(dash)
             print('{:<4s}'.format('ID'), '{:<10s}'.format('IMIE'), '{:<15s}'.format('NAZWISKO'))
@@ -56,7 +62,7 @@ def wyszukaj_autora():
         if choice == '2':
             os.system('cls')
             imie_autora = input('Wpisz IMIĘ Autora:')
-            query = (f'SELECT id_autora, imie, nazwisko FROM autorzy WHERE imie="{imie_autora}"')
+            query = f'SELECT id_autora, imie, nazwisko FROM autorzy WHERE imie="{imie_autora}"'
             mycursor.execute(query)
             print(dash)
             print('{:<4s}'.format('ID'), '{:<10s}'.format('IMIE'), '{:<15s}'.format('NAZWISKO'))
@@ -70,7 +76,7 @@ def wyszukaj_autora():
         if choice == '3':
             os.system('cls')
             nazwisko_autora = input('Wpisz MAZWISKO Autora:')
-            query = (f'SELECT id_autora, imie, nazwisko FROM autorzy WHERE nazwisko={nazwisko_autora}')
+            query = f'SELECT id_autora, imie, nazwisko FROM autorzy WHERE nazwisko="{nazwisko_autora}"'
             mycursor.execute(query)
             print(dash)
             print('{:<4s}'.format('ID'), '{:<10s}'.format('IMIE'), '{:<15s}'.format('NAZWISKO'))
@@ -90,7 +96,7 @@ def wyszukaj_autora():
             os.system('cls')
             menu_czytelnik()
     except:
-        input('Error occured. Press any key to return to main menu...')
+        input('Error occurred. Press any key to return to main menu...')
         main_menu()
 
 
@@ -105,7 +111,6 @@ def wyszukaj_ksiazki():
         print('3. Autor')
         print('4. Wydawnictwo')
         print('5. Gatunek')
-        # print('Opis')
         print('\nE. Exit')
         choice = input('\nOption: ')
         if choice == '1':
@@ -113,7 +118,6 @@ def wyszukaj_ksiazki():
             id_ksiazki = input('Wpisz ID książki:')
             query = f'SELECT * FROM ksiazki WHERE id_ksiazki={id_ksiazki}'
             mycursor.execute(query)
-
             print(dash)
             print('{:<4s}'.format('ID'), '{:<10s}'.format('ID Autora'), '{:<15s}'.format('WYDAWNICTWO'), '{:<10s}'.format('GATUNEK'), '{:<15s}'.format('TYTUŁ'))
             print(dash)
@@ -130,7 +134,7 @@ def wyszukaj_ksiazki():
 def menu_czytelnik():
     os.system('cls')
     print('BIBLIOTEKARZ (Beta)')
-    print('\nCzytelnik\n')
+    print('\nMenu - Czytelnik\n')
     print('1. Wyszukaj')
     print('\nE. Exit')
     choice = input('\nOption: ')
@@ -247,14 +251,13 @@ def main_menu():
         menu_bibliotekarz()
     elif choice.lower() == 'e':
         input('\nPress any key to close...')
-        exit(0)
+        exit()
     elif choice.lower() == 'exit':
         input('\nPress any key to close...')
-        exit(0)
+        exit()
     else:
         input('\nWrong input...')
         os.system('cls')
         main_menu()
-
 
 main_menu()
